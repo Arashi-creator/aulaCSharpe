622026737d1081034bec5ab8709e771a472ef379
Leonardo Vieira <leonardovieira295@gmail.com>
2025-02-26 14:54:04 -0300

    Repósitorio da aula C# 1

---
+++ AULA 01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AULA_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+           // úsuario escreve
+
+            Console.WriteLine("Escreva seu nome");
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Escreva sua idade");
+            string idadeUsuario = Console.ReadLine();
+
+            // você escreve
+
+             string nome = "Leonardo";
+            Console.Write("Escreva seu nome: ");
+            Console.WriteLine($"{nome}");
+             
+
+          
+        }
+    }
+}
+
